05e650ca42bc1eea3eec8d34b372170019434af6
manasa maddipatla <manasamaddipatla@gmail.com>
2023-03-11 16:31:13 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+def sample1(request):
+    return render(request,'sample.html')
+
+# Create your views here.
--- p7/urls.py
+++ p7/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import *
+from app1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sample/',sample,name='sample'),
+    path('sample1/',sample1,name='sample1'),
 ]
